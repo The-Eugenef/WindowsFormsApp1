af3259a3da3a63c773addd26ff0913d8f5a97eac
The-Eugenef <rodneyhalo@gmail.com>
2022-06-29 13:51:27 +0600

    рефакторинг

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -21,62 +21,16 @@ namespace WindowsFormsApp1
         int count;
         bool znak = true;
 
-        private void button9_Click(object sender, EventArgs e)
+        private void button17_Click_1(object sender, EventArgs e)
         {
-            textBox1.Text = textBox1.Text + 0;
+            Button B = (Button)sender;
+            if (textBox1.Text == "0")
+                textBox1.Text = B.Text;
+            else
+                textBox1.Text = textBox1.Text + B.Text;
         }
 
-        private void button10_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + 1;
-        }
-
-        private void button11_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + 2;
-        }
-
-        private void button12_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + 3;
-        }
-
-        private void button13_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + 4;
-        }
-
-        private void button14_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + 5;
-        }
-
-        private void button15_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + 6;
-        }
-
-        private void button16_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + 7;
-        }
-
-        private void button17_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + 8;
-        }
-
-        private void button18_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + 9;
-        }
-
-        private void button7_Click(object sender, EventArgs e)
-        {
-            textBox1.Text = textBox1.Text + ",";
-        }
-
-        private void button8_Click(object sender, EventArgs e)
+            private void button8_Click(object sender, EventArgs e)
         {
             if (znak == true)
             {
@@ -203,6 +157,8 @@ namespace WindowsFormsApp1
             label1.Text = x.ToString() + "^";
             znak = true;
         }
+
+        
 
         private void button6_Click(object sender, EventArgs e)
         {
